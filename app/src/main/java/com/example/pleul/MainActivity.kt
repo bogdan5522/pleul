b8dce6ee2d0b5b909b8dbaf7ee8ee0ca26622e57
bogdan5522 <kvantorium7@gmail.com>
2021-04-19 19:29:12 +0700

    j0ii90

---
+++ app/src/main/java/com/example/pleul/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.pleul
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button).setOnClickListener {
+            var intent = Intent(this, b22::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pleul/b22.kt
@@ -0,0 +1,11 @@
+package com.example.pleul
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class b22 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_b22)
+    }
+}
